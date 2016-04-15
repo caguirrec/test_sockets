a9a92f9c4e63b78d3a4fc45445e2691329174918
caguirrec <carlos.aguirre@taximo.co>
2016-04-15 18:13:06 -0500

    add test link

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -10,6 +10,7 @@ $this->pageTitle=Yii::app()->name;
 
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
+    <?php echo ('<li>'.CHtml::link('testSite').'</li>');?>
 	<li>View file: <code><?php echo __FILE__; ?></code></li>
 	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
 </ul>

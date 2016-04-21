407cd991ebfe2ad5bf711197dda318eba7f07ccd
caguirrec <carlos.aguirre@taximo.co>
2016-04-21 14:50:43 -0500

    test

--- protected/models/Alert.php
+++ protected/models/Alert.php
@@ -7,7 +7,7 @@
  */
 include("vendor/autoload.php");
 
-$server = \Ratchet\Server\IoServer::factory(
+$server = Ratchet\Server\IoServer::factory(
         new Ratchet\Http\HttpServer(
         new Ratchet\WebSocket\WsServer(        
         new kanguroAlert()))

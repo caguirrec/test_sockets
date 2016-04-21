6ad1939b37222500c7d571e04b6cd0c044e8868c
caguirrec <carlos.aguirre@taximo.co>
2016-04-21 15:04:24 -0500

    .

--- protected/models/Alert.php
+++ protected/models/Alert.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-include("vendor/autoload.php");
+//include("vendor/autoload.php");
 
 $server = Ratchet\Server\IoServer::factory(
         new Ratchet\Http\HttpServer(

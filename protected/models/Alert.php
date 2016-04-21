6ad7300b9e6aa2f7b05483976c646c1e45e44d87
caguirrec <carlos.aguirre@taximo.co>
2016-04-21 17:02:09 -0500

    .

--- protected/models/Alert.php
+++ protected/models/Alert.php
@@ -10,5 +10,5 @@ $server = Ratchet\Server\IoServer::factory(
         new Ratchet\Http\HttpServer(
         new Ratchet\WebSocket\WsServer(        
         new kanguroAlert()))
-        ,8080,'54.221.208.29');
+        ,8080,$_SERVER['SERVER_ADDR']);
 $server->run();

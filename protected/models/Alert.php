3c776b14f760e33552d9f68ddf54995690846ddb
caguirrec1 <carlos.aguirre@taximo.co>
2016-04-28 13:43:47 -0500

    .

--- protected/models/Alert.php
+++ protected/models/Alert.php
@@ -10,5 +10,5 @@ include("vendor/autoload.php");
 $server = Ratchet\Server\IoServer::factory(
         new \Ratchet\Http\HttpServer(
         new Ratchet\WebSocket\WsServer(new kanguroAlert()))
-        ,8080,'0.0.0.0');
+        ,8080,  gethostbyname(gethostname()));
 $server->run();

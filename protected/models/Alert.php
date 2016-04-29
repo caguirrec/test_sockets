99cfb0c5d0a25588e4aa46b492dd0409f2764e9f
caguirrec1 <carlos.aguirre@taximo.co>
2016-04-29 16:59:39 -0500

    .

--- protected/models/Alert.php
+++ protected/models/Alert.php
@@ -9,7 +9,8 @@
     require 'kanguroAlert.php';
     use Ratchet\ConnectionInterface;
     echo(gethostbyname(gethostname()));
-    $app = new Ratchet\App(gethostbyname(gethostname()),8080, '0.0.0.0');
-    $app->route('/chat', new Ratchet\Server\IoServer(new kanguroAlert()),array('*'));
-    $app->run();
+    $app = new Ratchet\App(gethostbyname(gethostname()), 8080, '0.0.0.0');
+    $app->route('/chat', new kanguroAlert(),array('*'));
+    $server = Ratchet\Server\IoServer::factory($app, 8080, '0.0.0.0');
+    $server->run();
     
